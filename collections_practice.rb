0ee5d171327e8167084e05e78cb28df552fd04ae
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 21:24:55 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,5 +10,11 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array) 
+  array.sort do |left, right|
+    left.length <=> right.length 
+  end 
+end
+
+def swap_elements(array) 
   
 end
