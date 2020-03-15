840eda6a5b34902e1c747e1386d8355de35d00de
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 21:33:34 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,53 @@
+def sort_array_asc(array) 
+  array.sort 
+end
 
+
+def sort_array_desc(array)
+  array.sort do |left, right| 
+    right <=> left 
+  end 
+end
+
+def sort_array_char_count(array) 
+  array.sort do |left, right|
+    left.length <=> right.length 
+  end 
+end
+
+def swap_elements(array) 
+  array[1], array[2] = array[2], array[1]
+  array 
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array) 
+  array.each do |item|
+    item[2] = "$"
+  end
+end 
+
+def find_a(array)
+  array.select do |word| 
+    word.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do |sum, x| 
+    sum + x 
+  end
+end 
+
+def add_s(array) 
+  array.collect do |word|
+    if array[1] == word 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end 
